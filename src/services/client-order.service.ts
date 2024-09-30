da81a594fac41fad84bba09b27e7053e704f19e6
Diman <pavlenkodim@mail.ru>
2024-09-30 19:43:48 +0500

    Initial in client-orders

---
+++ src/services/client-order.service.ts
@@ -0,0 +1,29 @@
+// task.service.ts
+
+import { HttpService, API_URL } from "./http.service";
+import store from "@/store";
+
+export class ClientOrderService {
+	static async getClientOrders(
+		_userid: number = 1
+		// _start: number = 0,
+		// _limit: number = 10
+	) {
+		let order_data;
+
+		try {
+			order_data = await HttpService.post({
+				url: `${API_URL}/GetClientOrders/v3/`,
+				params: {
+					userId: _userid.toString(),
+					// start: _start.toString(),
+					// limit: _limit.toString(),
+				},
+			});
+		} catch (error) {
+			console.error("Error:", error);
+		}
+
+		return order_data?.data;
+	}
+}
